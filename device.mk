2fbbdb2abf443178d819e2b3811b41287ae22ff8
skenakun <ajansyidad2014@gmail.com>
2026-05-10 02:36:30 +0700

    alioth: Welcome to Lunaris AOSP

--- device.mk
+++ device.mk
@@ -64,5 +64,8 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Inherit Private Keys
+-include vendor/lineage-priv/keys/keys.mk
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/alioth/alioth-vendor.mk)
