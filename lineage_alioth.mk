50224e2548cf72f75159e38febd339f0e51a50a5
skenakun <ajansyidad2014@gmail.com>
2026-07-18 00:35:02 +0000

    alioth: Welcome to AxionAOSP

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -20,12 +20,30 @@ TARGET_INCLUDES_MIUI_CAMERA := true
 
 # Enable UI enhancements
 TARGET_ENABLE_BLUR := true
+TARGET_NEEDS_VULKAN_MEDIA_FIX := true
 
 # Enable features
 TARGET_SUPPORTS_QUICK_TAP := true
 BYPASS_CHARGE_SUPPORTED := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 USE_PIXEL_CHARGING := true
+
+# AxionOS specific features
+TARGET_INCLUDE_AXFX := true
+
+# Camera information (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 48,8,5
+AXION_CAMERA_FRONT_INFO := 20
+TARGET_SUPPORTED_REFRESH_RATES := 60,120
+
+# Maintainer name (underscores become spaces in the UI)
+AXION_MAINTAINER := Yaseakun
+
+# Flashlight strength
+TORCH_STR_SUPPORTED := true
+
+# Processor name (underscores become spaces)
+AXION_PROCESSOR := Snapdragon_870_5G
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_alioth
